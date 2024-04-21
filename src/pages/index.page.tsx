4033fd66b901b1b1d4aa70467041ce0e03ccfb06
Daniel Cruz <danielcunha54@gmail.com>
2024-04-21 14:25:10 -0300

    Implement home and register pages

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Ignite Call</title>
-      </Head>
-      <main>
-        <Heading as="h1">Home</Heading>
-      </main>
-    </>
-  )
-}
+export { default } from './home'
