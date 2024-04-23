acbbe77829c938f0ecc7e90f24f8bb470c2922d7
Daniel Cruz <danielcunha54@gmail.com>
2024-04-23 19:45:22 -0300

    Implement confirm step form

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,79 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
+
+const confirmFormSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: 'O nome precisa ter no mínimo 3 caracteres.' }),
+  email: z.string().trim().email({ message: 'Digite um e-mail válido.' }),
+  observations: z.string().optional(),
+})
+
+type ConfirmFormValues = z.infer<typeof confirmFormSchema>
+
 export function ConfirmStep() {
-  return <div></div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmFormValues>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+
+  async function handleConfirmScheduling(data: ConfirmFormValues) {
+    console.log(data)
+  }
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          23 de abril de 2024
+        </Text>
+        <Text>
+          <Clock />
+          11:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" {...register('name')} />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="johndoe@example.com"
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,7 @@
 import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const step = 'calendar' as 'calendar' | 'confirm'
+  return step === 'calendar' ? <CalendarStep /> : <ConfirmStep />
 }
